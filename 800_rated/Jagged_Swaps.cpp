3dac583a8e7acd21bdc67654a0546be3b4cc941a
Zakir Ahmad Khan <khanzakirkhan723595@gmail.com>
2026-01-31 18:22:36 +0530

    jagged problem done

---
+++ 800_rated/Jagged_Swaps.cpp
@@ -0,0 +1,32 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+   ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t;
+    cin >> t;
+    while(t--) {
+        int n;
+        cin>>n;
+
+        vector<int> arr(n);
+        for(int i=0;i<n;i++)
+        {
+            cin>>arr[i];
+        }
+
+        
+        if (arr[0] == 1) {
+        cout << "YES" << endl;
+        } else {
+        cout << "NO" << endl;
+    }
+        
+            
+        
+    }
+    return 0; 
+}
